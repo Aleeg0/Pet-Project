bc874ecb7292cc98aa0d7a7c7e7230faf587c628
Kostya <betenya.kostya@mail.ru>
2023-12-18 17:08:21 +0300

    OtherForms

---
+++ Other Forms/LastForms/Unit1.pas
@@ -0,0 +1,47 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Edit1: TEdit;
+    Label5: TLabel;
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    BitBtn3: TBitBtn;
+    procedure BitBtn2Click(Sender: TObject);
+    procedure BitBtn3Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+uses Unit2, Unit3;
+
+procedure TForm1.BitBtn2Click(Sender: TObject);
+begin
+    Form2.ShowModal;
+end;
+
+procedure TForm1.BitBtn3Click(Sender: TObject);
+begin
+    Form3.ShowModal;
+end;
+
+end.
